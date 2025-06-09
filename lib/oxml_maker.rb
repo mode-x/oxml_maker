06c3933b35835dc1053d0448da53e6cd789b968b
Emmanuel Abia <abiaemma@gmail.com>
2025-06-09 19:40:03 +0100

    Initial setup

---
+++ lib/oxml_maker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "oxml_maker/version"
+
+module OxmlMaker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/oxml_maker/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OxmlMaker
+  VERSION = "0.1.0"
+end
