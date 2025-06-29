b08f6e48cc16a57cec114a598b311246430ba9ba
mode-x <abiaemma@gmail.com>
2025-06-29 08:55:47 +0100

    refactor: Add required core components (document, paragraph, table)

--- lib/oxml_maker.rb
+++ lib/oxml_maker.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "oxml_maker/version"
+require_relative "oxml_maker/document"
+require_relative "oxml_maker/paragraph"
+require_relative "oxml_maker/table"
 
 module OxmlMaker
   class Error < StandardError; end
