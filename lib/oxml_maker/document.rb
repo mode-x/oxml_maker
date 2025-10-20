19f6d742cc6088832b5b4e394cb5b4150107767a
airbearr <emmanuel.abia@corsearch.com>
2025-10-20 20:40:56 +0100

    refactor: Simplify document generation and improve file handling

--- lib/oxml_maker/document.rb
+++ lib/oxml_maker/document.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "zip"
+require "fileutils"
+
 # OxmlMaker module
 module OxmlMaker
   class Document
@@ -17,13 +20,7 @@ module OxmlMaker
         <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
         <w:document xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
           <w:body>
-            #{params[:sections].map do |section|
-              if section[:paragraph]
-                Oxml::Paragraph.new(section[:paragraph]).template
-              elsif section[:table]
-                Oxml::Table.new(section[:table]).template
-              end
-            end.join("\n")}
+            #{generate_sections}
             <w:sectPr>
               <w:pgSz w:w='#{params[:page_size][:width]}' w:h='#{params[:page_size][:height]}'/>
               <w:pgMar w:top='#{params[:page_margin][:top]}' w:right='#{params[:page_margin][:right]}' w:bottom='#{params[:page_margin][:bottom]}' w:left='#{params[:page_margin][:left]}' w:header='#{params[:page_margin][:header]}' w:footer='#{params[:page_margin][:footer]}' w:gutter='#{params[:page_margin][:gutter]}'/>
@@ -39,11 +36,22 @@ module OxmlMaker
       ensure_directories
       write_document_xml
       zip_docx_folder
+      convert_zip_to_docx
       copy_to_public
       cleanup_temp_files
     end
 
     private
+
+    def generate_sections
+      params[:sections].map do |section|
+        if section[:paragraph]
+          OxmlMaker::Paragraph.new(section[:paragraph]).template
+        elsif section[:table]
+          OxmlMaker::Table.new(section[:table]).template
+        end
+      end.join("\n")
+    end
 
     def prepare_paths
       @docx_root = File.join(__dir__, "docx")
@@ -67,7 +75,7 @@ module OxmlMaker
 
     def zip_docx_folder
       entries = Dir.glob(File.join(@docx_root, "**", "*"), File::FNM_DOTMATCH).select { |f| File.file?(f) }
-      Zip::File.open(@zip_path, Zip::File::CREATE) do |zipfile|
+      Zip::File.open(@zip_path, create: true) do |zipfile|
         entries.each do |file|
           rel_path = file.sub("#{@docx_root}/", "")
           zipfile.add(rel_path, file)
@@ -75,9 +83,27 @@ module OxmlMaker
       end
     end
 
-    def copy_to_public
+    def convert_zip_to_docx
       FileUtils.cp(@zip_path, @docx_path)
-      FileUtils.cp(@docx_path, File.join(Rails.root, "public", filename))
+    end
+
+    def copy_to_public(destination_dir = nil)
+      destination_dir ||= detect_public_dir
+      ensure_public_directory(destination_dir)
+      FileUtils.cp(@docx_path, File.join(destination_dir, filename))
+    end
+
+    def detect_public_dir
+      if defined?(Rails) && Rails.respond_to?(:root) && Rails.root
+        File.join(Rails.root, "public")
+      else
+        # For non-Rails environments (like testing), use local public directory
+        "public"
+      end
+    end
+
+    def ensure_public_directory(dir)
+      FileUtils.mkdir_p(dir)
     end
   end
 end
